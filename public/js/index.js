65eed3887c8b5fc306004d28471a3b31aa5aff98
muhammad <muhammadugv66@gmail.com>
2024-06-30 04:18:32 +0500

    added lazy loading image

--- public/js/index.js
+++ public/js/index.js
@@ -404,3 +404,31 @@ if (userBtn)
     const userId = this.getAttribute('data-user-id');
     roleManagement('user', userId);
   });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+if (imgTargets.length > 0) {
+  const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function () {
+      entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+  };
+
+  const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+  });
+
+  imgTargets.forEach((img) => imgObserver.observe(img));
+}
